faafe80f65d80b3d078a9f1c2e588aa4fe0e2869
talhashaikh5 <talha.shaikh5@gmail.com>
2022-01-09 12:21:59 +0530

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -77,7 +77,7 @@ def save_data(df, database_filename):
                     None
     """
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql('messages', engine, index=False)
+    df.to_sql('messages', engine, index=False, if_exists='replace')
     pass  
 
 
